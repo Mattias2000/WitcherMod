4c65ed7fcb46d013b69989dc8d31146504a67bc7
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2025-08-10 19:36:58 -0400

    moved the loot table and recipes to their correct spot
    Also got rid of the hardcoded loot table

--- eerstemod - kopie (2)/src/main/java/mattias/EersteMod/world/HerbGen.java
+++ eerstemod - kopie (2)/src/main/java/mattias/EersteMod/world/HerbGen.java
@@ -2,36 +2,32 @@ package mattias.EersteMod.world;
 
 import java.util.Random;
 
-import net.minecraft.block.BlockBush;
+import net.minecraft.block.BushBlock;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ISeedReader;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
 
-public class HerbGen extends WorldGenerator {
+public class HerbGen extends Feature<NoFeatureConfig> {
 
-	BlockBush flower;
-	public HerbGen(BlockBush flower)
+	BushBlock flower;
+	public HerbGen(BushBlock flower)
     {
-		this.flower=flower;
+        super(NoFeatureConfig.CODEC);
+        this.flower=flower;
     }
-	
-	
+
 	@Override
-	public boolean generate(World worldIn, Random rand, BlockPos position) {
-					
-		
-		
-	
-	
-            BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
+    public boolean generate(ISeedReader worldIn, ChunkGenerator generator, Random rand, BlockPos position, NoFeatureConfig config) {
+        BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
 
-			if (worldIn.isAirBlock(blockpos) && (!worldIn.provider.isNether() || blockpos.getY() < 255) && flower .canBlockStay(worldIn, blockpos, flower.getDefaultState()))
-            {
-                worldIn.setBlockState(blockpos, flower.getDefaultState(), 2);
-            }
-        
-
-        return true;
-	}
-
+        // Check if the block is air, not in the Nether, and can survive
+        if (worldIn.isAirBlock(blockpos) && worldIn.getWorld().getDimensionKey() != World.THE_NETHER && blockpos.getY() < 255 && flower.getDefaultState().isValidPosition(worldIn, blockpos)) {
+            worldIn.setBlockState(blockpos, flower.getDefaultState(), 2);
+            return true;
+        }
+        return false;
+    }
 }
